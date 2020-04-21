a90b5535822c34a3457931bb2edae52c2d6b5ae6
Muin Syed <mhc.msyed@gmail.com>
2020-04-20 22:41:10 -0400

    Took Grommet and Grommet Icons out of package.json because there were no easy or clear examples of laying out content with Grommet online. Added react-anime for animation of title and menu options.

--- src/App.js
+++ src/App.js
@@ -1,126 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Anime, { anime } from 'react-anime'
 
-import {
-    Anchor,
-    Box,
-    Button,
-    Collapsible,
-    Footer,
-    Grommet,
-    Heading,
-    Layer,
-    ResponsiveContext,
-    Text
-} from 'grommet'
+import Title from './Title/Title';
 
-import { FormClose, Notification } from 'grommet-icons'
+import './App.css'
 
-const theme = {
-    global: {
-        colors: {
-            brand: '#228BE6',
-        },
-        font: {
-            family: 'Roboto',
-            size: '18px',
-            height: '20px',
-        },
-    },
-}
-
-const AppBar = props => (
-    <Box
-        tag="header"
-        direction="row"
-        align="center"
-        justify="between"
-        background="brand"
-        pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-        elevation="medium"
-        style={{ zIndex: '1' }}
-        {...props}
-    />
+const App = props => (
+    <Anime delay={anime.stagger(500)} scale={[0.1, 0.9]} opacity={[0, 1]}>
+        <Title />
+        <div> Hello </div>
+        <div> Whats good</div>
+    </Anime>
 )
-
-const App = () => {
-    const [showSidebar, setShowSidebar] = useState(false)
-
-    return (
-        <Grommet theme={theme} full>
-            <ResponsiveContext.Consumer>
-                {size => (
-                    <Box fill>
-                        <AppBar>
-                            <Heading level="3" margin="none">
-                                My App
-                            </Heading>
-                            <Button
-                                icon={<Notification />}
-                                onClick={() => {
-                                    setShowSidebar(!showSidebar)
-                                }}
-                            />
-                        </AppBar>
-                        <Box
-                            direction="column"
-                            flex
-                            overflow={{ horizontal: 'hidden' }}
-                        >
-                            <Box flex align="center" justify="center">
-                                App Body
-                            </Box>
-                            {!showSidebar || size !== 'small' ? (
-                                <Collapsible
-                                    direction="horizontal"
-                                    open={showSidebar}
-                                >
-                                    <Box
-                                        flex
-                                        width="medium"
-                                        background="light-2"
-                                        elevation="small"
-                                        align="center"
-                                        justify="center"
-                                    >
-                                        Sidebar
-                                    </Box>
-                                </Collapsible>
-                            ) : (
-                                <Layer>
-                                    <Box
-                                        background="light-2"
-                                        tag="header"
-                                        justify="end"
-                                        align="center"
-                                        direction="row"
-                                    >
-                                        <Button
-                                            icon={<FormClose />}
-                                            onClick={() =>
-                                                setShowSidebar(false)
-                                            }
-                                        />
-                                    </Box>
-                                    <Box
-                                        fill
-                                        background="light-2"
-                                        align="center"
-                                        justify="center"
-                                    >
-                                        sidebar
-                                    </Box>
-                                </Layer>
-                            )}
-                            <Footer background="brand" pad="medium">
-                                <Text>Copyright</Text>
-                                <Anchor label="About" />
-                            </Footer>
-                        </Box>
-                    </Box>
-                )}
-            </ResponsiveContext.Consumer>
-        </Grommet>
-    )
-}
 
 export default App
