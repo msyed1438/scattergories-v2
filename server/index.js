ce4092d898dd50eeaf765dec1dd9a8e54516557e
Muin Syed <mhc.msyed@gmail.com>
2020-04-23 23:38:14 -0400

    Changed package.json 'name' from 'react-coding-challenge' to 'scattergories-v2'. Added socketio to server and socketio-client to React client in App.js. Made connection to the socket, and console.log'd 'yooooo!" inside useEffect to demonstrate successful ping and pong to the socket server.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+var http = require('http').createServer(app)
+var io = require('socket.io')(http)
 
 app.use('/', express.static(path.join(__dirname, '../dist')))
 
@@ -9,4 +11,9 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+io.on('connection', socket => {
+    console.log('a user connected')
+    socket.emit('yoo')
+})
+
+http.listen(port, () => console.log(`App listening on port ${port}!`))
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import socketIOClient from 'socket.io-client'
+const ENDPOINT = 'http://127.0.0.1:3000'
 
 import HomePage from './HomePage/HomePage'
 import CreateGamePage from './CreateGamePage/CreateGamePage'
@@ -7,20 +9,29 @@ import JoinGamePage from './JoinGamePage/JoinGamePage'
 
 import './App.css'
 
-const App = props => (
-    <Router>
-        <Switch>
-            <Route path="/create-game">
-                <CreateGamePage />
-            </Route>
-            <Route path="/join-game">
-                <JoinGamePage />
-            </Route>
-            <Route exact path="/">
-                <HomePage />
-            </Route>
-        </Switch>
-    </Router>
-)
+const App = () => {
+    useEffect(() => {
+        const socket = socketIOClient(ENDPOINT)
+        socket.on('yoo', data => {
+            console.log('YOOOOOOO!')
+        })
+    }, [])
+
+    return (
+        <Router>
+            <Switch>
+                <Route path="/create-game">
+                    <CreateGamePage />
+                </Route>
+                <Route path="/join-game">
+                    <JoinGamePage />
+                </Route>
+                <Route exact path="/">
+                    <HomePage />
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
 
 export default App
