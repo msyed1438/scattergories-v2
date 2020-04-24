8b4753cc7e768f92c2f98026866c75dd257aeb2d
Muin Syed <mhc.msyed@gmail.com>
2020-04-23 21:03:11 -0400

    Added fallback URL in Express server (/*), installed React Redux and Redux libraries, and added a darker gradient for background.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const path = require('path');
+const path = require('path')
 
 app.use('/', express.static(path.join(__dirname, '../dist')))
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
