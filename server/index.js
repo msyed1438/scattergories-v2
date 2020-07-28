9c39c82325ccd738fc0b3b552eb13457b4cf20aa
Muin Syed <mhc.msyed@gmail.com>
2020-07-27 20:41:35 -0400

    Cleared test components. Adjusted css for GameRoom Players component.

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const path = require('path')
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.use('/', express.static(path.join(__dirname, '../dist')))
+app.use('/', express.static(path.join(__dirname, '../dist')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'))
-})
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
 
-const Room = require('./gameRoundClasses/gameRoundClass')
+const Room = require('./gameRoundClasses/gameRoundClass');
 
 const gameState = {
     rooms: {},
-}
+};
 
 //Mock Data:
 
@@ -23,124 +23,124 @@ gameState.rooms['testRoom'] = new Room('testRoom', [
     'Foods',
     'Songs',
     'Countries',
-])
-gameState.rooms.testRoom.addPlayer('Muin', 123)
-gameState.rooms.testRoom.addPlayer('Soud', 456)
-gameState.rooms.testRoom.addPlayer('Mahir', 789)
+]);
+gameState.rooms.testRoom.addPlayer('Muin', 123);
+gameState.rooms.testRoom.addPlayer('Soud', 456);
+gameState.rooms.testRoom.addPlayer('Mahir', 789);
 
 gameState.rooms['anotherRoom'] = new Room('anotherRoom', [
     'Cartoons',
     'Artists',
     'Computer Science',
-])
-gameState.rooms.anotherRoom.addPlayer('MewMew', 123)
-gameState.rooms.anotherRoom.addPlayer('Choyon', 456)
-gameState.rooms.anotherRoom.addPlayer('Riham', 789)
+]);
+gameState.rooms.anotherRoom.addPlayer('MewMew', 123);
+gameState.rooms.anotherRoom.addPlayer('Choyon', 456);
+gameState.rooms.anotherRoom.addPlayer('Riham', 789);
 
 //Helper functions
 
-const getListOfRooms = () => Object.keys(gameState.rooms)
-const getRoomCategories = roomName => gameState.rooms[roomName].categories
-const getRoomPlayers = roomName => Object.keys(gameState.rooms[roomName].room)
+const getListOfRooms = () => Object.keys(gameState.rooms);
+const getRoomCategories = roomName => gameState.rooms[roomName].categories;
+const getRoomPlayers = roomName => Object.keys(gameState.rooms[roomName].room);
 
 io.on('connection', clientSocket => {
-    console.log('A user connected...')
-    console.log('The user id is: ', clientSocket.id)
+    console.log('A user connected...');
+    console.log('The user id is: ', clientSocket.id);
 
     clientSocket.on('disconnect', () => {
-        console.log(`User ${clientSocket.username} disconnected`)
+        console.log(`User ${clientSocket.username} disconnected`);
 
         if (clientSocket.roomName !== undefined) {
             gameState.rooms[clientSocket.roomName].removePlayer(
                 clientSocket.username
-            )
+            );
 
             io.in(clientSocket.roomName).emit(
                 'UPDATE_PLAYERS',
                 getRoomPlayers(clientSocket.roomName)
-            )
+            );
 
             const roomIsEmpty =
                 Object.keys(gameState.rooms[clientSocket.roomName].room)
-                    .length === 0
+                    .length === 0;
 
             if (roomIsEmpty) {
-                delete gameState.rooms[clientSocket.roomName]
-                clientSocket.broadcast.emit('UPDATE_ROOMS', getListOfRooms())
+                delete gameState.rooms[clientSocket.roomName];
+                clientSocket.broadcast.emit('UPDATE_ROOMS', getListOfRooms());
             }
         }
-    })
+    });
 
     clientSocket.on('SET_SOCKET_USERNAME', username => {
-        clientSocket.username = username
-        console.log('Client username configured...')
-        console.log('The clients username: ', clientSocket.username)
-    })
+        clientSocket.username = username;
+        console.log('Client username configured...');
+        console.log('The clients username: ', clientSocket.username);
+    });
 
     clientSocket.on('CREATE_GAME_ROOM', roomConfig => {
-        console.log('A room was created. Here is the config: ', roomConfig)
-        console.log('Creating new room...')
+        console.log('A room was created. Here is the config: ', roomConfig);
+        console.log('Creating new room...');
 
-        const { roomName, categories } = roomConfig
+        const { roomName, categories } = roomConfig;
 
-        clientSocket.roomName = roomName
-        clientSocket.join(roomName)
+        clientSocket.roomName = roomName;
+        clientSocket.join(roomName);
 
-        gameState.rooms[roomName] = new Room(roomName, categories)
-        console.log('Room was added, here is our game state now: ', gameState)
+        gameState.rooms[roomName] = new Room(roomName, categories);
+        console.log('Room was added, here is our game state now: ', gameState);
 
         gameState.rooms[roomName].addPlayer(
             clientSocket.username,
             clientSocket.id
-        )
+        );
 
         console.log(
             'Player was added. Here is our roomState now: ',
             gameState.rooms[roomName]
-        )
+        );
 
-        clientSocket.broadcast.emit('UPDATE_ROOMS', getListOfRooms())
-    })
+        clientSocket.broadcast.emit('UPDATE_ROOMS', getListOfRooms());
+    });
 
     clientSocket.on('JOIN_GAME_ROOM', roomName => {
-        console.log(`Player ${clientSocket.username} has joined the room. `)
-        clientSocket.join(roomName)
-        clientSocket.roomName = roomName
+        console.log(`Player ${clientSocket.username} has joined the room. `);
+        clientSocket.join(roomName);
+        clientSocket.roomName = roomName;
         // clientSocket.emit('ROOM_CATEGORIES', getRoomCategories(roomName))
-        gameState.rooms[roomName].addPlayer(clientSocket.username)
+        gameState.rooms[roomName].addPlayer(clientSocket.username);
         io.to(clientSocket.roomName).emit(
             'UPDATE_PLAYERS',
             getRoomPlayers(roomName)
-        )
-        console.log('Here is gameState: ', gameState.rooms)
+        );
+        console.log('Here is gameState: ', gameState.rooms);
         console.log(
             'Here is the ',
             roomName,
             ' room name in the game state: ',
             gameState.rooms[roomName]
-        )
+        );
         console.log(
             'Here are the players in room [',
             roomName,
             ']:  ',
             Object.keys(gameState.rooms[roomName].room)
-        )
-    })
+        );
+    });
 
     clientSocket.on('GET_PLAYERS', roomName => {
         console.log(
             'Client has requested all players in the room. Here they are: ',
             getRoomPlayers(clientSocket.roomName)
-        )
+        );
         io.in(roomName).emit(
             'UPDATE_PLAYERS',
             getRoomPlayers(clientSocket.roomName)
-        )
-    })
+        );
+    });
 
     clientSocket.on('GET_LIST_OF_ROOMS', () => {
-        clientSocket.emit('LIST_OF_ROOMS', getListOfRooms())
-    })
-})
+        clientSocket.emit('LIST_OF_ROOMS', getListOfRooms());
+    });
+});
 
-http.listen(port, () => console.log(`App listening on port ${port}!`))
+http.listen(port, () => console.log(`App listening on port ${port}!`));
--- src/test-components/MountDismount.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './MountDismount.css'
-
-const MountDismount = ({ isShowing }) => {
-    show ? <div style={{ animation: `fadeIn 1s` }}>HELLO</div> : null
-}
-
-export default MountDismount
