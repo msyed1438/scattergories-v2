3584d46a1bcc9767f1f473456dab2efb30100cde
Muin Syed <mhc.msyed@gmail.com>
2020-09-26 21:07:17 -0400

    Added Redux flow for SET_CATEGORIES. Action creator, action type, and reducer (which does the same as CREATE_CATEGORIES) were created.

--- src/reducks/ducks/index.js
+++ src/reducks/ducks/index.js
@@ -1,7 +1,8 @@
 // Actions
-const CREATE_ROOM = 'CREATE_ROOM'
-const CREATE_CATEGORIES = 'CREATE_CATEGORIES'
-const JOIN_ROOM = 'JOIN_ROOM'
+const CREATE_ROOM = 'CREATE_ROOM';
+const CREATE_CATEGORIES = 'CREATE_CATEGORIES';
+const JOIN_ROOM = 'JOIN_ROOM';
+const SET_CATEGORIES = 'SET_CATEGORIES';
 
 //Initial State
 const initialState = {
@@ -12,62 +13,74 @@ const initialState = {
     gameIsInSession: false,
     gameEntries: {},
     roundData: {},
-}
+};
 
 // Reducer
 export default function reducer(state = initialState, action = {}) {
     switch (action.type) {
         case CREATE_ROOM: {
-            const { username, roomName, numberOfCategories } = action.payload
+            const { username, roomName, numberOfCategories } = action.payload;
 
             return {
                 ...state,
                 username,
                 roomName,
                 numberOfCategories,
-            }
+            };
         }
         case CREATE_CATEGORIES: {
-            const categories = action.payload
+            const categories = action.payload;
 
             return {
                 ...state,
                 categories,
-            }
+            };
+        }
+
+        case SET_CATEGORIES: {
+            const categories = action.payload;
+
+            return {
+                ...state,
+                categories,
+            };
         }
 
         case JOIN_ROOM: {
-            const { username, roomName } = action.payload
+            const { username, roomName } = action.payload;
 
             return {
                 ...state,
                 username,
                 roomName,
-            }
+            };
         }
 
         default:
-            return state
+            return state;
     }
 }
 
 // Action Creators
 export function createRoom(roomConfig) {
-    return { type: CREATE_ROOM, payload: roomConfig }
+    return { type: CREATE_ROOM, payload: roomConfig };
 }
 
 export function createCategories(categories) {
-    return { type: CREATE_CATEGORIES, payload: categories }
+    return { type: CREATE_CATEGORIES, payload: categories };
 }
 
 export function joinGameRoom(roomConfig) {
-    return { type: JOIN_ROOM, payload: roomConfig }
+    return { type: JOIN_ROOM, payload: roomConfig };
 }
 
+export function setCategories(categories) {
+    return { type: SET_CATEGORIES, payload: categories };
+}
 
 // side effects, only as applicable
 // e.g. thunks, epics, etc
 export function getWidget() {
     return dispatch =>
-        get('/widget').then(widget => dispatch(updateWidget(widget)))
+        get('/widget').then(widget => dispatch(updateWidget(widget)));
 }
