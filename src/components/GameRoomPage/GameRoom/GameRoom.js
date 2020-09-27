e1cc1ae9e5991b5d7c8e089d61318e878749746b
Muin Syed <mhc.msyed@gmail.com>
2020-09-26 21:02:55 -0400

    Upon the client socket detecting 'disconnect' (within the useEffect callback function), the history hook from React Router 'pushes' the user back to the homepage route '/'. Also added CSS Grid layout for GameRoomComponent using .grid-container.

--- src/components/GameRoomPage/GameRoom/GameRoom.js
+++ src/components/GameRoomPage/GameRoom/GameRoom.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import socket from '../../socketInstance';
+import { useHistory } from 'react-router-dom';
 
-import Players from './Players'
-import Categories from './Categories';
+import Players from './Players';
+import CategoriesSubmission from './Categories';
 
 const GameRoom = () => {
+
+    const history = useHistory();
+
+    useEffect(() => {
+        socket.on('disconnect', () => {
+            history.push('/')
+        });
+    }, []);
+
     return (
         <div className="page-container">
             <div className="game-room-container">
-                <div className="room-info-container">
+                <div className="grid-container">
+                    <div className="game-table">Foo</div>
                     <Players />
-                    <Categories />
+                    <div className="room-name">Room Name: Mewmew Squad</div>
+                    <div className="letter-active">Intermission . . .</div>
+                    <CategoriesSubmission />
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default GameRoom
+export default GameRoom;
