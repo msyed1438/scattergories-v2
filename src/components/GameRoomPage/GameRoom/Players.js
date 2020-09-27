35bcbd0ad8a62428d20e3b2aaab9c7464c86e550
Muin Syed <mhc.msyed@gmail.com>
2020-09-26 21:06:05 -0400

    Eliminated earlier div classes to focus on functionalty. Prettier reformatted  to now add semicolons.

--- src/components/GameRoomPage/GameRoom/Players.js
+++ src/components/GameRoomPage/GameRoom/Players.js
@@ -1,33 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 
-import socket from '../../socketInstance'
+import socket from '../../socketInstance';
 
 const Players = () => {
-    const [players, setPlayers] = useState([])
-    const roomName = useSelector(state => state.roomName)
+    const [players, setPlayers] = useState([]);
+    const roomName = useSelector(state => state.roomName);
 
     useEffect(() => {
-        socket.emit('GET_PLAYERS', roomName)
+        socket.emit('GET_PLAYERS', roomName);
         socket.on('UPDATE_PLAYERS', players => {
-            console.log('Here are the players: ', players)
-            setPlayers(players)
-        })
-    }, [])
+            console.log('Here are the players: ', players);
+            setPlayers(players);
+        });
+    }, []);
 
     return (
+        // <div className="room-players">
+        //     <h3 className="players-header"> PLAYERS </h3>
+        //     <div className="players-container">
+        //         {players.map(player => (
+        //             <li className="player" key={player}>
+        //                 {player}
+        //             </li>
+        //         ))}
+        //     </div>
+        // </div>
         <div className="room-players">
-            <h3 className="players-header"> PLAYERS </h3>
-            <div className="players-container">
+            <h3> PLAYERS </h3>
+            <div>
                 {players.map(player => (
-                    <li className="player" key={player}>
-                        {player}
-                    </li>
+                    <li key={player}>{player}</li>
                 ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Players
+export default Players;
