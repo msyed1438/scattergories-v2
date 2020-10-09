c721ea6f9897841678e3009716e35a1df3722431
Muin Syed <mhc.msyed@gmail.com>
2020-10-08 23:41:25 -0400

    Added conditional rendering for GameRoom component where if Redux state property gameIsActive is true, render the pencil animation and categories form, else render the alarm bell animation and round table. The Redux state is toggeled and updated using socket.on('UPDATE_GAME_STATUS') and dispatching with React-Redux hooks.

--- src/components/GameRoomPage/GameRoom/GameRoom.js
+++ src/components/GameRoomPage/GameRoom/GameRoom.js
@@ -5,9 +5,17 @@ import { useHistory } from 'react-router-dom';
 import Players from './Players';
 import CategoriesSubmission from './Categories';
 import RoundTable from './RoundTable';
+import GameInProgressLogo from './GameInProgressLogo';
+import IntermissionInProgressLogo from './IntermissionInProgress';
+
+import { setGameActive } from '../../../reducks/ducks/index';
+import { useDispatch, useSelector } from 'react-redux';
 
 const GameRoom = () => {
     const history = useHistory();
+    const roomName = useSelector(state => state.roomName);
+    const gameActive = useSelector(state => state.gameActive);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         socket.on('disconnect', () => {
@@ -15,15 +23,26 @@ const GameRoom = () => {
         });
     }, []);
 
+    useEffect(() => {
+        socket.emit('GET_GAME_STATUS', roomName);
+        socket.on('UPDATE_GAME_STATUS', gameActive => {
+            dispatch(setGameActive(gameActive));
+        });
+    }, []);
+
     return (
         <div className="page-container">
             <div className="game-room-container">
                 <div className="grid-container">
-                    <RoundTable />
+                    {gameActive ? <GameInProgressLogo /> : <RoundTable />}
+                    {gameActive ? (
+                        <CategoriesSubmission />
+                    ) : (
+                        <IntermissionInProgressLogo />
+                    )}
                     <Players />
                     <div className="room-name">Room Name: Mewmew Squad</div>
                     <div className="letter-active">Intermission . . .</div>
-                    <CategoriesSubmission />
                 </div>
             </div>
         </div>
