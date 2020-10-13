35445952de75f263748772cabf4352191f220bbf
Muin Syed <mhc.msyed@gmail.com>
2020-10-12 22:21:00 -0400

    Adjusted transition for less horizontal movement due to frames stuttering.

--- src/components/AnimationWrappers/RouteTransitionWrapper/RouteTransitionWrapper.js
+++ src/components/AnimationWrappers/RouteTransitionWrapper/RouteTransitionWrapper.js
@@ -3,9 +3,8 @@ import Anime from 'react-anime'
 
 const transition = {
     duration: 1500,
-    scale: [1.5, 1.0],
+    scale: [0.25, 1.0],
     opacity: [0, 1],
-    translateX: ['45vh', 0],
 }
 
 export default props => <Anime {...transition} {...props} />
