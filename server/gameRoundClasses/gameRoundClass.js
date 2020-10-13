07d43ce81f7dab0ac21ac6b1449ca96fea37ca3b
Muin Syed <mhc.msyed@gmail.com>
2020-10-12 22:17:31 -0400

    Added helper functions getRoundEntries (which returns the collected data from each player for that round), hasAllPlayerSubmissions (which checks to see that all players have sent in their entries), and resetPlayerSubmissions (which clears the game state for players entries)

--- server/gameRoundClasses/gameRoundClass.js
+++ server/gameRoundClasses/gameRoundClass.js
@@ -1,38 +1,39 @@
 class Player {
-    constructor(name, socketId) { //TODO: Check if socketId defaulting to null should be allowed in the future
-        this.name = name
-        this.socketId = socketId
-        this.wordsPlayed = []
-        this.points = 0
+    constructor(name, socketId) {
+        //TODO: Check if socketId defaulting to null should be allowed in the future
+        this.name = name;
+        this.socketId = socketId;
+        this.wordsPlayed = null;
+        this.points = 0;
     }
 
     addPoints(number) {
-        this.points += number
+        this.points += number;
     }
 
     increment() {
-        this.points++
+        this.points++;
     }
 
     decrement() {
-        this.points--
+        this.points--;
     }
 
-    addWordsToWordsPlayed(words) {
-        this.wordsPlayed.push(words)
+    setWordsPlayed(words) {
+        this.wordsPlayed = words;
     }
 
     subtractPoints(number) {
-        this.points -= number
+        this.points -= number;
     }
 }
 
 class Room {
     constructor(roomName, categories) {
-        this.roomName = roomName,
-        this.room = {},
-        this.gameIsActive = false,
-        this.lettersNotPlayedSoFar = [
+        (this.roomName = roomName),
+            (this.room = {}),
+            (this.gameIsActive = false), //TODO: Add toggle class method
+            (this.lettersNotPlayedSoFar = [
                 'a',
                 'b',
                 'c',
@@ -52,28 +53,53 @@ class Room {
                 'r',
                 's',
                 't',
-                'w'
-            ],
-        this.categories = categories
+                'w',
+            ]),
+            (this.categories = categories);
     }
 
-    addPlayer(playerName, playerSocketId = null) { //TODO: Check if socketId defaulting to null should be allowed in the future
-        console.log('Here is the playerName: ', playerName)
-        this.room[playerName] = new Player(playerName, playerSocketId)
+    addPlayer(playerName, playerSocketId = null) {
+        //TODO: Check if socketId defaulting to null should be allowed in the future
+        console.log('Here is the playerName: ', playerName);
+        this.room[playerName] = new Player(playerName, playerSocketId);
     }
 
     removePlayer(playerName) {
-        delete this.room[playerName]
+        delete this.room[playerName];
     }
 
     pickRandomLetter() {
-        const getRandomInt = max => Math.floor(Math.random() * Math.floor(max))
-        const numberOfLettersLeft = this.lettersNotPlayedSoFar.length
-        const randomIndex = getRandomInt(numberOfLettersLeft)
+        const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+        const numberOfLettersLeft = this.lettersNotPlayedSoFar.length;
+        const randomIndex = getRandomInt(numberOfLettersLeft);
 
-        const randomLetter = this.lettersNotPlayedSoFar.splice(randomIndex, 1)
-        return randomLetter
+        const randomLetter = this.lettersNotPlayedSoFar.splice(randomIndex, 1);
+        return randomLetter;
+    }
+
+    getRoundEntries() {
+        return Object.values(this.room).map(({ name, wordsPlayed }) => {
+            return { name, wordsPlayed };
+        });
+    }
+
+    hasAllPlayersSubmissions() {
+        const isNotEmpty = value => value !== null;
+
+        const playerSubmissions = this.getRoundEntries();
+
+        if (playerSubmissions.map(({ wordsPlayed }) => wordsPlayed).every(isNotEmpty)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    resetPlayerSubmissions() {
+        for (var player in this.room) {
+            this.room[player].wordsPlayed = null
+        }
     }
 }
 
-module.exports = Room
+module.exports = Room;
