39b85b6bac0517bab67d3ae414db18c6c81d1170
Muin Syed <mhc.msyed@gmail.com>
2020-10-12 22:45:19 -0400

    Changed GameRoom table from hardcoded example to dynamic rows and headers based on game state.

--- src/components/GameRoomPage/GameRoom/RoundTable.js
+++ src/components/GameRoomPage/GameRoom/RoundTable.js
@@ -1,40 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+
+import socket from '../../socketInstance';
+
+const RoundRow = ({ name, wordsPlayed }) => {
+    return (
+        <tr>
+            <td>{name}</td>
+            {wordsPlayed.map(word => (
+                <td key={word}>{word}</td>
+            ))}
+        </tr>
+    );
+};
 
 const RoundTable = () => {
     const categories = useSelector(state => state.categories);
+    const [roundData, setRoundData] = useState([]);
+
+    useEffect(() => {
+        socket.on('TOTAL_ROUND_DATA', roundData => {
+            // console.log(
+            //     'Here is the round data received from the server: ',
+            //     roundData
+            // );
+
+            setRoundData(roundData);
+        });
+    }, []);
 
     return (
         <div className="game-table">
             <table id="game-table">
                 <thead>
                     <tr>
+                        <th>Name</th>
                         {categories.map(category => (
                             <th key={category}>{category}</th>
                         ))}
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Maria Anders</td>
-                        <td>Germany</td>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Alfreds Futterkiste</td>
-                    </tr>
-                    <tr>
-                        <td>Berglunds snabbköp</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Alfreds Futterkiste</td>
-                    </tr>
+                    {roundData.map(({ name, wordsPlayed }) => (
+                        <RoundRow
+                            key={name}
+                            name={name}
+                            wordsPlayed={wordsPlayed ? wordsPlayed : ['']}
+                        />
+                    ))}
                 </tbody>
             </table>
         </div>
